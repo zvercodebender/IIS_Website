b56221b34ba7be68e3e78bc1a787ccf3a5624be8
Admin <rbroker@digital.ai>
2020-12-17 15:45:36 -0500

    update to use build number

--- build.sh
+++ build.sh
@@ -10,11 +10,12 @@ zip -r ../iisroot.zip ./*
 cd ../..
 if [ ! -z "${GET_LOCAL}" ]
 then
-  xl apply --file iis_website.yaml --values version=$VERSION
+  GITHUB_RUN_NUMBER="0"
+  xl apply --file iis_website.yaml --values version=${VERSION}-${GITHUB_RUN_NUMBER}
 else
   curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
   chmod +x xl
-  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file iis_website.yaml --values version=$VERSION
+  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file iis_website.yaml --values version=${VERSION}-${GITHUB_RUN_NUMBER}
 fi
 rm artifacts/iisroot.zip
 if [ -z "${GET_LOCAL}" ]
